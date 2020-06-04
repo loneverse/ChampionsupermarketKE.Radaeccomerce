592ec82f15ab2c669846f1918e84146bd5ab5883
Peggysymons <ayub>
2020-06-04 19:28:02 +0300

    Updated views

--- supermarket/views.py
+++ supermarket/views.py
@@ -63,7 +63,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url = ''
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
