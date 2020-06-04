23ac10474fa08e7fdfd37a15c94ca10bc3418276
Peggysymons <ayub>
2020-06-04 15:38:21 +0300

    Updated STATIC_ROOT

--- champion/settings.py
+++ champion/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_ROUTE = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROUTE = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
