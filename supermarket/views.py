bac02976fe48ea88b798d5e68f6d19ef2b90f593
Peggysymons <ayub>
2020-06-04 14:06:08 +0300

    Initial Commit

---
+++ supermarket/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView ,DeleteView
+from django.contrib.auth.decorators import login_required
+from .models import Post
+
+@login_required()
+def shop(request):
+
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'supermarket/shop.html', context)
+
+
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'supermarket/shop.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+
+class UserPostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'supermarket/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = ''
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+@login_required()
+def products(request):
+    return render(request, 'supermarket/products.html', {'title': 'products'})
+
+@login_required()
+def feedback(request):
+    return render(request, 'supermarket/Feedback.html', {'title': 'feedback'})
+
+
