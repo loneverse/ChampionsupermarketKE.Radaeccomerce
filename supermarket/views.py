371840cb44869abc266a1e86f122644b874338b1
Peggysymons <ayub>
2020-06-05 19:50:07 +0300

    Updated

--- supermarket/views.py
+++ supermarket/views.py
@@ -14,7 +14,7 @@ def shop(request):
     return render(request, 'supermarket/shop.html', context)
 
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'supermarket/shop.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
@@ -22,7 +22,7 @@ class PostListView(ListView):
     paginate_by = 2
 
 
-class UserPostListView( ListView):
+class UserPostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'supermarket/user_posts.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
