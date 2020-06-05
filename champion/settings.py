9866fbd3a054b056012220fdef96edb0eca51b1d
Peggysymons <ayub>
2020-06-05 12:56:53 +0300

    Updated Settings

--- champion/settings.py
+++ champion/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -151,3 +152,5 @@ DEFAULT_FROM_EMAIL = 'ayubngash7@gmail.com'
 EMAIL_HOST_USER = 'ayubngash7@gmail.com'
 SERVER_EMAIL = 'ayubngash7@gmail.com'
 EMAIL_HOST_PASSWORD = 'mjvzkulblkttfaro'
+
+django_heroku.settings(locals())
